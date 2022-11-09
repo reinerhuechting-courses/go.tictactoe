9259c4323d854502e93c59934351e0a9f84a95ba
Reiner Hüchting <dev@reinerhuechting.de>
2022-11-09 22:12:01 +0100

    Tests angepasst und erweitert.

--- boardchecks_test.go
+++ boardchecks_test.go
@@ -2,17 +2,52 @@ package main
 
 import "fmt"
 
-func testboard1() [][]string {
+func testboardGeneric() [][]string {
 	return [][]string{
 		{"*", " ", " "},
 		{"*", "*", " "},
 		{"*", "*", "*"}}
 }
 
+func testboardX1() [][]string {
+	return [][]string{
+		{" ", "X", " "},
+		{"O", "X", "X"},
+		{"O", "X", "O"}}
+}
+
+func testboardX2() [][]string {
+	return [][]string{
+		{" ", "O", " "},
+		{"X", "X", "X"},
+		{"O", "X", "O"}}
+}
+
+func testboardO1() [][]string {
+	return [][]string{
+		{" ", "O", " "},
+		{"X", "O", "O"},
+		{"X", "O", "X"}}
+}
+
+func testboardO2() [][]string {
+	return [][]string{
+		{" ", "X", " "},
+		{"O", "O", "O"},
+		{"X", "O", "X"}}
+}
+
+func testboardDraw() [][]string {
+	return [][]string{
+		{"O", "X", "X"},
+		{"X", "X", "O"},
+		{"O", "O", "X"}}
+}
+
 func ExampleRowContainsOnly() {
-	fmt.Println(RowContainsOnly(testboard1(), 0, "*"))
-	fmt.Println(RowContainsOnly(testboard1(), 1, "*"))
-	fmt.Println(RowContainsOnly(testboard1(), 2, "*"))
+	fmt.Println(RowContainsOnly(testboardGeneric(), 0, "*"))
+	fmt.Println(RowContainsOnly(testboardGeneric(), 1, "*"))
+	fmt.Println(RowContainsOnly(testboardGeneric(), 2, "*"))
 
 	// Output:
 	// false
@@ -21,9 +56,9 @@ func ExampleRowContainsOnly() {
 }
 
 func ExampleColumnContainsOnly() {
-	fmt.Println(ColumnContainsOnly(testboard1(), 0, "*"))
-	fmt.Println(ColumnContainsOnly(testboard1(), 1, "*"))
-	fmt.Println(ColumnContainsOnly(testboard1(), 2, "*"))
+	fmt.Println(ColumnContainsOnly(testboardGeneric(), 0, "*"))
+	fmt.Println(ColumnContainsOnly(testboardGeneric(), 1, "*"))
+	fmt.Println(ColumnContainsOnly(testboardGeneric(), 2, "*"))
 
 	// Output:
 	// true
@@ -32,10 +67,40 @@ func ExampleColumnContainsOnly() {
 }
 
 func ExampleNumberOfOccurrences() {
-	fmt.Println(NumberOfOccurrences(testboard1(), " "))
-	fmt.Println(NumberOfOccurrences(testboard1(), "*"))
+	fmt.Println(NumberOfOccurrences(testboardGeneric(), " "))
+	fmt.Println(NumberOfOccurrences(testboardGeneric(), "*"))
 
 	// Output:
 	// 3
 	// 6
+}
+
+func ExamplePlayerXWins() {
+	fmt.Println(PlayerXWins(testboardX1()))
+	fmt.Println(PlayerXWins(testboardX2()))
+	fmt.Println(PlayerXWins(testboardO1()))
+	fmt.Println(PlayerXWins(testboardO2()))
+	fmt.Println(PlayerXWins(testboardDraw()))
+
+	// Output:
+	// true
+	// true
+	// false
+	// false
+	// false
+}
+
+func ExamplePlayerOWins() {
+	fmt.Println(PlayerOWins(testboardX1()))
+	fmt.Println(PlayerOWins(testboardX2()))
+	fmt.Println(PlayerOWins(testboardO1()))
+	fmt.Println(PlayerOWins(testboardO2()))
+	fmt.Println(PlayerOWins(testboardDraw()))
+
+	// Output:
+	// false
+	// false
+	// true
+	// true
+	// false
 }
