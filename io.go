0ffa2201725a064de1caa63d4ddd3fc7ce60c583
Reiner Hüchting <dev@reinerhuechting.de>
2022-11-09 22:47:09 +0100

    Spielfeld-Ausgabe implementiert.

--- io.go
+++ io.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Erwartet ein Spielfeld und gibt es auf der Konsole aus.
 // Dabei sollen die Zeilen des Spielfelds einzeln ausgegeben und
 // geeignete Trennzeichen verwendet werden (siehe Tests).
 func PrintBoard(board [][]string) {
-	fmt.Println(board)
+	fmt.Println("+---+---+---+")
+	for _, row := range board {
+		fmt.Println("|", strings.Join(row, " | "), "|")
+		fmt.Println("+---+---+---+")
+	}
 }
 
 // Erwartet einen Spieler-String und ein Spielfeld.
