5a397017c2bc22f68b788a4a95e5d0a41362ab18
Reiner Hüchting <dev@reinerhuechting.de>
2022-11-10 10:51:38 +0100

    IO-funktionen auf zwei Dateien aufgeteilt.

--- gameio.go
+++ gameio.go
@@ -2,19 +2,7 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
-
-// Erwartet ein Spielfeld und gibt es auf der Konsole aus.
-// Dabei sollen die Zeilen des Spielfelds einzeln ausgegeben und
-// geeignete Trennzeichen verwendet werden (siehe Tests).
-func PrintBoard(board [][]string) {
-	fmt.Println("+---+---+---+")
-	for _, row := range board {
-		fmt.Println("|", strings.Join(row, " | "), "|")
-		fmt.Println("+---+---+---+")
-	}
-}
 
 // Erwartet einen Spieler-String und ein Spielfeld.
 // Fragt den Spieler nach seinem Zug und führt diesen aus,
